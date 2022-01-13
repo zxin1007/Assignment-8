50996c97e336ab1273c46ca6448ac16b87bbaa65
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-13 16:12:08 -0500

    added the react app and added a barebones grid

---
+++ dom-3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Grid from './Grid';
+
+function App() {
+  return (
+    <main>
+      <Grid />    
+    </main>
+  );
+}
+
+export default App;
---
+++ dom-3/src/Grid.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Grid(props){
+    return(
+        <>
+        
+        <div className="grid-container">
+            <button className="cell"></button>
+            <button className="cell"></button>
+            <button className="cell"></button>
+            <button className="cell"></button>
+        </div>
+        </>
+
+    )
+}
+
+export default Grid
