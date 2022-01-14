83d5acaf23b95534387cf8a0d22af859adedf234
Oleksii Kravtsov <bahbok1@gmail.com>
2022-01-14 11:25:34 -0500

    Pretty much did all the necessary UI. should be ready to start working on the functionality. closed #1.

--- dom-3/src/Controls.js
+++ dom-3/src/Controls.js
@@ -5,9 +5,14 @@ class Controls extends React.Component{
         return(
             <div className="controls">
                 <button className="control">Add Row</button>
+                <button className="control">+10</button>
                 <button className="control">Remove Row</button>
+                <button className="control">-10</button>
                 <button className="control">Add Column</button>
+                <button className="control">+10</button>
                 <button className="control">Remove Column</button>
+                <button className="control">-10</button>
+                <div className="empty">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</div>
                 <button className="control">Colors</button>
                 <button className="control">Fill Empties</button>
                 <button className="control">Fill All</button>
