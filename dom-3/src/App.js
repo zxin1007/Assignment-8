de23761e59e2c291703ce0f7200825f386979166
Zixin Zou <zxinmm@gmail.com>
2022-01-14 15:59:33 -0500

    add/remove

--- dom-3/src/App.js
+++ dom-3/src/App.js
@@ -8,8 +8,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      column:5,
-      row:4
+      column:0,
+      row:0
     };
 
     this.handleClick = this.handleClick.bind(this)
@@ -17,7 +17,24 @@ class App extends React.Component {
 
   handleClick(item, count) {
       console.log(item)
-      console.log(3+count)
+      console.log(count)
+      if (item==="row"){
+        if (this.state.row===0 && this.state.column===0 && count>0){
+          this.setState({column:1, row:1})
+        }else if (this.state.row+count>0){
+          this.setState({...this.state, row:this.state.row+count})
+        } else{
+          this.setState({column:0, row:0})
+        }
+      } else{
+        if (this.state.row===0 && this.state.column===0 && count>0){
+          this.setState({column:1, row:1})
+        }else if (this.state.column+count>0){
+          this.setState({...this.state, column:this.state.column+count})
+        } else{
+          this.setState({column:0, row:0})
+        }
+      }
   }
 
   render(){
--- dom-3/src/Controls.js
+++ dom-3/src/Controls.js
@@ -1,17 +1,21 @@
 import React from "react"
 
 class Controls extends React.Component{
+
+
     render(){
+        
         return(
             <div className="controls">
-                <button className="control">Add Row</button>
-                <button className="control">+10</button>
-                <button className="control">Remove Row</button>
-                <button className="control">-10</button>
-                <button className="control">Add Column</button>
-                <button className="control">+10</button>
-                <button className="control">Remove Column</button>
-                <button className="control">-10</button>
+                <button className="control" onClick={()=>this.props.onClick("row", +1)} >Add Row</button>
+                <button className="control"onClick={()=>this.props.onClick("row", +10)} >+10</button>
+                <button className="control" onClick={()=>this.props.onClick("row", -1)}>Remove Row</button>
+                <button className="control" onClick={()=>this.props.onClick("row", -10)}>-10</button>
+                <button className="control" onClick={()=>this.props.onClick("column", +1)} >Add Column</button>
+                <button className="control" onClick={()=>this.props.onClick("column", +10)}>+10</button>
+                <button className="control" onClick={()=>this.props.onClick("column", -1)}>Remove Column</button>
+                <button className="control" onClick={()=>this.props.onClick("column", -10)}>-10</button>
+         
                 <div className="empty">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</div>
                 <button className="control">Colors</button>
                 <button className="control">Fill Empties</button>
